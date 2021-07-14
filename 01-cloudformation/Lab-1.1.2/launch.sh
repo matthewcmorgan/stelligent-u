e1abeaddff1edc76154461a2d72dc107a43aae3d
Matthew Morgan <matthew.morgan@stelligent.com>
2021-07-14 16:33:04 -0700

    wip

--- 01-cloudformation/Lab-1.1.2/launch.sh
+++ 01-cloudformation/Lab-1.1.2/launch.sh
@@ -3,6 +3,5 @@
 STACKNAME="mcm-01-cloudformation"
 TESTFILE="file://MinimalS3.yml"
 PARAMFILE="file://s3params.yml"
-BUCKETNAMEVAR="$STACKNAME_$(date +"%m%d%Y%H%M")"
 
 aws cloudformation update-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters $PARAMFILE
