f7c5a0e87ac24da788db9dedcebcef31d66fecd1
Matthew Morgan <matthew.morgan@stelligent.com>
2021-07-14 01:11:34 -0700

    more wip

--- 01-cloudformation/Lab-1.1.2/launch.sh
+++ 01-cloudformation/Lab-1.1.2/launch.sh
@@ -2,7 +2,7 @@
 # Use this script as a quick execute shim for testing cfn stacks
 STACKNAME="mcm-01-cloudformation"
 TESTFILE="file://MinimalS3.yml"
-
+PARAMFILE="file://s3params.yml"
 BUCKETNAMEVAR="$STACKNAME_$(date +"%m%d%Y%H%M")"
 
-aws cloudformation update-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters ParameterKey=BucketNameVar,ParameterValue=$BUCKETNAMEVAR
+aws cloudformation update-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters $PARAMFILE
