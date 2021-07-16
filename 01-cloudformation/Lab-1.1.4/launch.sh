975762904c1e11386ac4da2ff07ec8e0940fdcfe
Matthew Morgan <matthew.morgan@stelligent.com>
2021-07-16 10:28:41 -0700

    wip

--- 01-cloudformation/Lab-1.1.4/launch.sh
+++ 01-cloudformation/Lab-1.1.4/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Use this script as a quick execute shim for testing cfn stacks
-STACKNAME="mcm-01-cloudformation-Lab4"
-TESTFILE="file://S3viaConditionalPsuedoParams.yml"
-PARAMFILE="file://s3params.json"
-REGION='us-east-2'
+STACKNAME="mcm-01-cloudformation-Lab5"
+TESTFILE="file://IAMUserAndPolicy.yml"
+PARAMFILE="file://IAMparams.json"
+REGION='us-west-2'
 
 aws cloudformation create-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters $PARAMFILE --region $REGION
 
