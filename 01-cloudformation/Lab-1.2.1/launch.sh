1d7c8e076838202c4251684a8e3d2851b858fbdb
Matthew Morgan <matthew.morgan@stelligent.com>
2021-07-16 18:38:20 -0700

    lab 1.2.1 working

--- 01-cloudformation/Lab-1.2.1/launch.sh
+++ 01-cloudformation/Lab-1.2.1/launch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Use this script as a quick execute shim for testing cfn stacks
-STACKNAME="mcm-01-cloudformation-Lab4"
-TESTFILE="file://S3viaConditionalPsuedoParams.yml"
-PARAMFILE="file://s3params.json"
-REGION='us-east-2'
+STACKNAME="mcm-01-cloudformation-Lab5"
+TESTFILE="file://IAMUserAndPolicy.yml"
+PARAMFILE="file://IAMparams.json"
+REGION='us-west-2'
+CAPABILITIES='CAPABILITY_NAMED_IAM'
 
-aws cloudformation create-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters $PARAMFILE --region $REGION
+aws cloudformation create-stack --stack-name $STACKNAME --template-body $TESTFILE --parameters $PARAMFILE --region $REGION --capabilities $CAPABILITIES 
 
 # aws cloudformation update-termination-protection --stack-name $STACKNAME --enable-termination-protection | --no-enable-termination-protection
 
